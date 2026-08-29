73ff2ee853cacc9fbaf2b32af977e8104570fe93
git_weijianjun <2661043132>
2026-08-29 14:01:33 +0800

    修改本地开发服务器域名

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3000
-  }
-})
+    host: '127.0.0.1',
+    port: 3000,
+  },
+});
